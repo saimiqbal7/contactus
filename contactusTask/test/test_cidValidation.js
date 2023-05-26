0240dd13a1e795fd9bb52a139e43c8dfb266a4c3
soma <soma@koii.network>
2023-05-26 11:57:21 -0300

    update test_cidValidation file

--- contactusTask/test/test_cidValidation.js
+++ contactusTask/test/test_cidValidation.js
@@ -3,66 +3,147 @@ const nacl = require('tweetnacl');
 const db = require('../db_model');
 const bs58 = require('bs58');
 
-let submission_value = "bafybeig5322lknop4u6m4p26jd4bcp7rdbwkuqy3ifeytqrmo2ogia5kwe"
-let round = 1000
+// TEST hardcode the submission_value and round
+let submission_value = "bafybeih34osgxqa4euewbmieili74xcn6adb6inc26kmz4pegbqj4vw3hi" // <= this is the CID of the Contact submission
+let round = 7 // <= hardcode round
+
 async function test_cidValidation(submission_value) {
-  console.log('******/  TEST contact CID VALIDATION Task FUNCTION /******');
+  console.log('******/ TEST contact CID VALIDATION Task FUNCTION /******');
   const outputraw = await dataFromCid(submission_value);
   const output = outputraw.data;
+  console.log('OUTPUT', output);
+  console.log('RESPONSE DATA length', output.proofs.length);
+  console.log('PUBLIC KEY', output.node_publicKey);
+  console.log('SIGNATURE', output.node_signature);
 
-  const proofs_list_object = output.proofs;
-  console.log('RESPONSE DATA', proofs_list_object);
-  const publicKey = output.node_publicKey;
-  console.log('PUBLIC KEY', publicKey);
-  const signature = output.node_signature;
-  console.log('SIGNATURE', signature);
+  // Check that the node who submitted the proofs is a valid staked node
+  let isNode = await verifyNode(
+    output.proofs,
+    output.node_signature,
+    output.node_publicKey,
+  );
+  console.log("Is the node's signature on the CID payload correct?", isNode);
 
-  let isNode = await verifyNode(proofs_list_object, signature, publicKey);
-  console.log('IS NODE True?', isNode);
+  // check each item in the contacts list and verify that the node is holding that payload, and the signature matches
+  let isContact = await verifyContacts(output.proofs);
+  console.log('IS Contact True?', isContact);
 
-  const AuthUserList = [];
-  let iscontact = await verifycontact(proofs_list_object, AuthUserList);
-  console.log('Authenticated Users List:', AuthUserList);
-  console.log('IS contact True?', iscontact);
-
-  if (isNode && iscontact) return true; // if both are true, return true
+  if (isNode && isContact) return true; // if both are true, return true
   else return false; // if one of them is false, return false
 }
 
-async function verifycontact(proofs_list_object, AuthUserList) {
+async function verifyContacts(proofs_list_object) {
   let allSignaturesValid = true;
+  let AuthUserList = await db.getAllAuthLists();
+  console.log('Authenticated Users List:', AuthUserList);
+
+  // TODO: this part need to be fixed
+
   for (const proofs of proofs_list_object) {
-    console.log('proofs.value.publicKey', proofs.value[0].publicKey);
-    const contact_object = await db.getcontact(proofs.value[0].publicKey);
-    console.log('contact OBJECT', contact_object);
-    const messageUint8Array = new Uint8Array(
-      Buffer.from(JSON.stringify(contact_object.data)),
-    );
-    const signature = proofs.value[0].signature;
-    console.log('SIGNATURE', signature);
-    const publicKey = proofs.value[0].publicKey;
-    console.log('PUBLIC KEY', publicKey);
-    const signatureUint8Array = bs58.decode(signature);
-    const publicKeyUint8Array = bs58.decode(publicKey);
+    let publicKey = proofs.value[0].publicKey;
 
-    // verify the contact signature
-    const isSignatureValid = await verifySignature(
-      messageUint8Array,
-      signatureUint8Array,
-      publicKeyUint8Array,
-    );
-    console.log('IS SIGNATURE VALID', isSignatureValid);
+    // call other nodes to get the node list
+    const nodeUrlList = await namespaceWrapper.getNodes();
 
-    if (isSignatureValid) {
-      AuthUserList.push(publicKey);
-    } else {
-      allSignaturesValid = false;
+    // TEST hardcode the node list
+    // const nodeUrlList = [
+    //   "http://localhost:10000",
+    // ]
+
+    // verify the signature of the Contact for each nodes
+    for (const nodeUrl of nodeUrlList) {
+      console.log('cheking Contact on ', nodeUrl);
+
+      // get all Contact in this node
+      const res = await axios.get(
+        `${nodeUrl}/task/${TASK_ID}/Contact/get/${publicKey}`,
+      );
+
+      // TEST hardcode the node endpoint
+      // const res = await url axios.get(`${nodeUrl}/Contact/get/${publicKey}`);
+
+      // check node's status
+      if (res.status != 200) {
+        console.error('ERROR', res.status);
+        continue;
+      }
+
+      // get the payload
+      const contact = res.data;
+
+      // check if the user's pubkey is on the authlist
+      if (AuthUserList.hasOwnProperty(contact.publicKey)) {
+        console.log('User is on the auth list');
+      } else {
+        
+        // Check if the public key is an ETH address
+        if (contact.publicKey.length == 42) { 
+
+          // Verify the ETH signature
+          const { data, publicKey, signature } = contact;
+
+          // Decode the signature
+          const signatureBuffer = bs58.decode(signature);
+          const r = signatureBuffer.slice(0, 32);
+          const s = signatureBuffer.slice(32, 64);
+          const v = signatureBuffer.slice(64);
+
+          // Hash the message
+          const message = JSON.stringify(data);
+          const messageHash = web3.utils.keccak256(message);
+
+          // Recover the signer's public key
+          const publicKeyRecovered = ethUtil.ecrecover(
+            ethUtil.toBuffer(messageHash),
+            v[0],
+            r,
+            s,
+          );
+
+          // Convert the recovered public key to an Ethereum address
+          const recoveredAddress = ethUtil.bufferToHex(
+            ethUtil.pubToAddress(publicKeyRecovered),
+          );
+
+          // Check if the recovered address matches the provided public key
+          if (recoveredAddress.toLowerCase() === publicKey.toLowerCase()) {
+            console.log('Payload signature is valid');
+            await db.setAuthList(publicKey);
+          } else {
+            console.log('Payload signature is invalid');
+            allSignaturesValid = false;
+          }
+
+        } else {
+
+          // Verify the signature
+          const messageUint8Array = new Uint8Array(
+            Buffer.from(JSON.stringify(contact.data)),
+          );
+          const signature = contact.signature;
+          const publicKey = contact.publicKey;
+          const signatureUint8Array = bs58.decode(signature);
+          const publicKeyUint8Array = bs58.decode(publicKey);
+          const isSignatureValid = await verifySignature(
+            messageUint8Array,
+            signatureUint8Array,
+            publicKeyUint8Array,
+          );
+          console.log(`IS SIGNATURE ${publicKey} VALID?`, isSignatureValid);
+
+          if (isSignatureValid) {
+            await db.setAuthList(publicKey);
+          } else {
+            allSignaturesValid = false;
+          }
+        }
+      }
     }
   }
   return allSignaturesValid;
 }
 
-async function verifyNode(proofs_list_object, signature, publicKey) {    
+async function verifyNode(proofs_list_object, signature, publicKey) {
   const messageUint8Array = new Uint8Array(
     Buffer.from(JSON.stringify(proofs_list_object)),
   );
